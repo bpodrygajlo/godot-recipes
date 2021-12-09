dcdb02c9806b2f5f5a8d68a3f677506f9834320d
bpodrygajlo <bartosz.podrygajlo@gmail.com>
2021-12-09 08:23:05 +0100

    Change Bezier from singleton to Class

--- common/bezier.gd
+++ common/bezier.gd
@@ -1,12 +1,13 @@
-extends Node
+extends Reference
+class_name Bezier
 
 
 
-func quadratic(p0: Vector2, p1: Vector2, p2: Vector2, t: float) -> Vector2:
+static func quadratic(p0: Vector2, p1: Vector2, p2: Vector2, t: float) -> Vector2:
 	return (1 - t) * ((1 - t) * p0 + t * p1) + t * ((1 - t) * p1 + t * p2)
 
 
-func quadratic_segments(p0 : Vector2, p1 : Vector2, p2: Vector2, nr_of_segments : int) -> Array:
+static func quadratic_segments(p0 : Vector2, p1 : Vector2, p2: Vector2, nr_of_segments : int) -> Array:
 	var segment_borders = [{"pos": p0, "length" : 0}]
 	var total_len : float = 0.0
 	var prev_point = quadratic(p0, p1, p2, 0)
