09210f381d307dea46d4d833f88e903af4d21c70
bpodrygajlo <bartosz.podrygajlo@gmail.com>
2021-12-19 20:29:16 +0100

    Viewport position to world position

---
+++ viewport_to_world/Camera2D.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		print(get_viewport_transform().xform_inv(event.position) == get_global_mouse_position())
